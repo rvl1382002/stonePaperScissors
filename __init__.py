f812c384290f8135904317566b50ea62eb719415
Riddhish <44116452+rvl1382002@users.noreply.github.com>
2022-02-09 12:54:09 +0530

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,21 +1,9 @@
 import functions
-import players
-
 
 if __name__ == '__main__':
-    functions.homeWindow()
     try:
-        ans1=functions.ob1.x
-        print(ans1)
-        if (ans1 == 1):
-            print("Help clicked")
-
-        elif (ans1 == 2):
-            print("ABout us clicked")
-        elif (ans1==3):
-            print("Single Player clicked")
-        else:
-            print("Double Player clicked")
+        functions.homeWindow()
     except:
-
         print("Thanks for visiting")
+    finally:
+        print("Thank-you")
